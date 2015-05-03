12b3244be530cb02ae4fda68ac6c27f115df7775
mykhailoivankiv <neformal.lviv@gmail.com>
2015-05-03 17:15:28 +0300

    Changed style.

--- src/js/components/Maze.js
+++ src/js/components/Maze.js
@@ -6,13 +6,56 @@ var b = BEM.b("maze");
 class Maze extends React.Component {
   constructor (pref) {
     super();
-    this.state = {};
+    this.state = {
+      maze: [
+        [
+          { top: true, left: true,  bottom: false, right: false},
+          { top: true, left: false, bottom: false, right: false},
+          { top: true, left: true, bottom: false, right: false},
+          { top: true, left: false, bottom: true, right: false},
+          { top: true, left: false, bottom: false, right: false},
+          { top: true, left: false, bottom: false, right: false},
+          { top: true, left: false, bottom: false, right: false},
+          { top: true, left: false, bottom: false, right: false},
+          { top: true, left: false, bottom: false, right: false},
+          { top: true, left: false, bottom: false, right: false},
+          { top: true, left: false, bottom: false, right: false},
+          { top: true, left: false, bottom: false, right: false},
+          { top: true, left: false, bottom: false, right: true }
+        ],
+        [
+          { top: false, left: true,  bottom: false, right: false},
+          { top: false, left: false, bottom: false, right: false},
+          { top: false, left: true, bottom: false, right: false},
+          { top: false, left: false, bottom: true, right: false},
+          { top: false, left: false, bottom: false, right: false},
+          { top: false, left: false, bottom: false, right: false},
+          { top: false, left: false, bottom: false, right: false},
+          { top: false, left: false, bottom: false, right: false},
+          { top: false, left: false, bottom: false, right: false},
+          { top: false, left: false, bottom: false, right: false},
+          { top: false, left: false, bottom: false, right: false},
+          { top: false, left: false, bottom: false, right: false},
+          { top: false, left: false, bottom: false, right: true }
+        ]
+
+      ]
+    };
   }
 
   render () {
+    var {maze} = this.state;
     return (
       <div className={b()}>
-        Maze!
+
+        {maze.map((row => <div className={b("row")}>
+          {row.map((cell, index) => <div className = {b("cell",{
+            top: cell.top,
+            left: cell.left,
+            bottom: cell.bottom,
+            right: cell.right
+           })}>{index}</div>)}
+        </div>))}
       </div>
     )
   };
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,4 @@
 import React from "react";
-
-
 import Maze from "components/Maze"
-//window.newGame = new Game();
 
 React.render(<Maze />, document.body);
