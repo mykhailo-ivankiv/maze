fd3bc89bed983c7cb25240d2da1be0ce77fbc4e9
Mykhailo Ivankiv <neformal.lviv@gmail.com>
2015-05-20 20:11:38 +0300

    Updated indentation for gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -9,46 +9,46 @@ var plumber = require("gulp-plumber");
 
 
 gulp.task("process-styles", function (){
-    gulp.src([
-      "src/**/*.scss",
-      "!src/**/*tmp*.scss"
-    ])
-        .pipe(plumber())
-        .pipe(sourcemaps.init())
-            .pipe(sass())
-            .pipe(autoprefixer())
+  gulp.src([
+    "src/**/*.scss",
+    "!src/**/*tmp*.scss"
+  ])
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(autoprefixer())
 
-        .pipe(sourcemaps.write("."))
-        .pipe(plumber.stop())
-        .pipe(gulp.dest("dist"))
+    .pipe(sourcemaps.write("."))
+    .pipe(plumber.stop())
+    .pipe(gulp.dest("dist"))
 });
 
 gulp.task("process-scripts", function (){
-    gulp.src(["src/**/*.js"])
-        .pipe(plumber())
-        .pipe(sourcemaps.init())
-        .pipe(babel({modules: "amd", optional: ["es7.classProperties"], blacklist: ["useStrict"]}))
-        .pipe(sourcemaps.write("."))
-        .pipe(plumber.stop())
-        .pipe(gulp.dest("dist"));
+  gulp.src(["src/**/*.js"])
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(babel({modules: "amd", optional: ["es7.classProperties"], blacklist: ["useStrict"]}))
+    .pipe(sourcemaps.write("."))
+    .pipe(plumber.stop())
+    .pipe(gulp.dest("dist"));
 });
 
 
 gulp.task("process-html", function (){
-    gulp.src("src/**/*.html")
-        .pipe(gulp.dest("dist"));
+  gulp.src("src/**/*.html")
+    .pipe(gulp.dest("dist"));
 });
 
 gulp.task("clean", function(cb) {
-    del(["dist/"], cb);
+  del(["dist/"], cb);
 });
 
 gulp.task("default", ["clean"], function () { //Build for deployment.
-    gulp.start( "process-scripts", "process-styles", "process-html");
+  gulp.start( "process-scripts", "process-styles", "process-html");
 });
 
 gulp.task("watch", function () {
-    gulp.watch("src/**/*.js", ["process-scripts"]);
-    gulp.watch("src/**/*.scss", ["process-styles"]);
-    gulp.watch("src/**/*.html", ["process-html"]);
+  gulp.watch("src/**/*.js", ["process-scripts"]);
+  gulp.watch("src/**/*.scss", ["process-styles"]);
+  gulp.watch("src/**/*.html", ["process-html"]);
 });
